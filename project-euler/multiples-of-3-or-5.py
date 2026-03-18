a71cb7267bcbf051b6d0fcc167ff13a1d4df0552
MuizZatam <muizzzatam@gmail.com>
2026-03-18 12:39:36 +0530

    feat: optimize multiples of 3 or 5 using a closed formula

--- project-euler/multiples-of-3-or-5.py
+++ project-euler/multiples-of-3-or-5.py
@@ -1,3 +1,26 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
-print(sum([i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]))
+
+def sum_of_multiples(n, m):
+
+    first_term = 1
+    number_of_terms = m // n
+    last_term = number_of_terms
+
+    return n * ((number_of_terms / 2) * (first_term + last_term))
+
+
+def main():
+
+    sum_of_multiples_of_3 = sum_of_multiples(3, 999)
+    sum_of_multiples_of_5 = sum_of_multiples(5, 999)
+
+    sum_of_multiples_of_15 = sum_of_multiples(15, 999)
+
+    total = sum_of_multiples_of_3 + sum_of_multiples_of_5 - sum_of_multiples_of_15
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
